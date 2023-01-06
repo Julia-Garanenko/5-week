99b122e59023571d401dd7f31ebea7b352abb6a8
Julia <garanenko.julia.22@gmail.com>
2023-01-06 22:46:01 +0200

    changed JS (+HTML, CSS)

--- js/index.js
+++ js/index.js
@@ -12,11 +12,18 @@ let days = [
 ];
 let day = days[now.getDay()];
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
-
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 dayTime.innerHTML = `${day} ${hours}:${minutes}`;
 
 function displayWeather(response) {
+  console.log(response.data);
+
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#currentDegree");
   temperatureElement.innerHTML = `${temperature}`;
@@ -28,6 +35,16 @@ function displayWeather(response) {
   let wind = response.data.wind.speed;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = ` ${wind}`;
+  let weatherDescr = response.data.weather[0].description;
+  let weatherDescrElement = document.querySelector("#weather-descr");
+  weatherDescrElement.innerHTML = `${weatherDescr}`;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchWeather() {
