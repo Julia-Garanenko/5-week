3b0a778a258784ad80af0f2480cabb3cb33ac343
Julia <garanenko.julia.22@gmail.com>
2023-01-11 17:17:31 +0200

    function displayForecast + HTML

--- js/index.js
+++ js/index.js
@@ -21,6 +21,41 @@ if (minutes < 10) {
 }
 dayTime.innerHTML = `${day} ${hours}:${minutes}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let daysForecast = [
+    "Monday",
+    "Tuesday",
+    "Wednsday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  daysForecast.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+       <div class="col-2">
+         <div class="weather-forecast-date">${day}</div>
+         <img
+           src="http://openweathermap.org/img/wn/50d@2x.png"
+           alt=""
+           width="42"
+         />
+         <div class="weather-forecast-temperatures">
+           <span class="weather-forecast-temperature-max"> 18° </span>
+           <span class="weather-forecast-temperature-min"> 12° </span>
+         </div>
+       </div>
+     `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeather(response) {
   console.log(response.data);
 
@@ -72,3 +107,5 @@ function searchCurrentWeather() {
 
 let linkCurrent = document.querySelector("#button-current");
 linkCurrent.addEventListener("click", searchCurrentWeather);
+
+displayForecast();
