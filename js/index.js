43db96e58bab4e81fa860fd1e234d209c30e4063
Julia <garanenko.julia.22@gmail.com>
2023-01-12 19:46:06 +0200

    Revert "improved displayForecast (everyday temp, icons + function formatDay)"
    This reverts commit 88607bc18d463a5188ae8ea785aee1c3f9964eec.

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ let days = [
   "Thursday",
   "Friday",
   "Saturday",
+  "Sunday",
 ];
 let day = days[now.getDay()];
 let hours = now.getHours();
@@ -28,36 +29,35 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
-  let forecast = response.data.daily;
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-
-  forecast.forEach(function (forecastDay, index) {
-    if ((index > 0, index < 6)) {
-      forecastHTML =
-        forecastHTML +
-        `
+  let daysForecast = [
+    "Monday",
+    "Tuesday",
+    "Wednsday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  daysForecast.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
        <div class="col-2">
-         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+         <div class="weather-forecast-date">${day}</div>
          <img
-           src="http://openweathermap.org/img/wn/${
-             forecastDay.weather[0].icon
-           }@2x.png"
+           src="http://openweathermap.org/img/wn/50d@2x.png"
            alt=""
            width="42"
          />
          <div class="weather-forecast-temperatures">
-           <span class="weather-forecast-temperature-max"> ${Math.round(
-             forecastDay.temp.max
-           )}° </span>
-           <span class="weather-forecast-temperature-min"> ${Math.round(
-             forecastDay.temp.min
-           )}° </span>
+           <span class="weather-forecast-temperature-max"> 18° </span>
+           <span class="weather-forecast-temperature-min"> 12° </span>
          </div>
        </div>
      `;
-    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
